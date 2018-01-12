389e1bb7b1841bc6be0088de567d25f98a172fcb
hsp96 <34785730+hsp96@users.noreply.github.com>
2018-01-13 00:39:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,8 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
-        return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
-
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
-
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    
+    speech = "Hello"
 
     print("Response:")
     print(speech)
